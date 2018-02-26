b14ec0cf12d9e6e79f6f1b7ac2b830027e58896e
shaff5 <sean.shaffer@hotmail.com>
2018-02-25 20:02:50 -0500

    trivial change to test github commit

--- PropertyManagement.WebApi/Controllers/BuildingController.cs
+++ PropertyManagement.WebApi/Controllers/BuildingController.cs
@@ -63,6 +63,7 @@
             b.AddressLine1 = building.AddressLine1;
             b.City = building.City;
             b.ZipCode = building.ZipCode;
+            //trivial change
 
             return Ok(b);
         }
