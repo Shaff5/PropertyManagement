1cc85b44289cce176635157db4c858f885527dd5
shaff5 <sean.shaffer@hotmail.com>
2018-03-29 22:41:52 -0400

    Upgrade to Angular 5

--- PropertyManagement/ClientApp/app/app.module.shared.ts
+++ PropertyManagement/ClientApp/app/app.module.shared.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
@@ -24,7 +24,7 @@ import { BuildingDetailComponent } from './components/buildings/building-detail.
     ],
     imports: [
         CommonModule,
-        HttpModule,
+        HttpClientModule,
         FormsModule,
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
--- PropertyManagement/ClientApp/app/components/buildings/building.service.ts
+++ PropertyManagement/ClientApp/app/components/buildings/building.service.ts
@@ -1,4 +1,4 @@
-﻿import { Http } from '@angular/http';
+﻿import { HttpClient } from '@angular/common/http';
 import { Injectable, Inject } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
@@ -9,34 +9,34 @@ import { Building } from './building';
 export class BuildingService {
     private apiUrl: string;
 
-    constructor(private http: Http, @Inject('API_URL') apiUrl: string) {
+    constructor(private httpClient: HttpClient, @Inject('API_URL') apiUrl: string) {
         this.apiUrl = apiUrl;
     }
 
     getBuildings() {
-        return this.http.get(this.apiUrl + 'Building')
-            .map(result => result.json() as Building[])
+        return this.httpClient.get<Building[]>(this.apiUrl + 'Building')
+            .map(result => result)
             .catch(this.handleError);
     }
 
     getBuilding(id: number) : Observable<Building> {
-        return this.http.get(this.apiUrl + 'Building/' + id)
-            .map(result => result.json() as Building)
+        return this.httpClient.get<Building>(this.apiUrl + 'Building/' + id)
+            .map(result => result)
             .catch(this.handleError);
     }
 
     addBuilding(building: Building) {
-        return this.http.post(this.apiUrl + 'Building/', building)
+        return this.httpClient.post(this.apiUrl + 'Building/', building)
             .map(() => null).catch(this.handleError);
     }
 
     updateBuilding(id: number, building: Building) {
-        return this.http.put(this.apiUrl + 'Building/' + id, building)
+        return this.httpClient.put(this.apiUrl + 'Building/' + id, building)
             .map(() => null).catch(this.handleError);
     }
     
     deleteBuilding(id: number) {
-        return this.http.delete(this.apiUrl + 'Building/' + id).map(() => null).catch(this.handleError);
+        return this.httpClient.delete(this.apiUrl + 'Building/' + id).map(() => null).catch(this.handleError);
     }
 
     private handleError(error: any): Observable<any> {
