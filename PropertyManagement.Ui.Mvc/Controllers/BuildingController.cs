0ff73d22d74040ff3e86b8c22481e73d6c740fec
shaff5 <sean.shaffer@hotmail.com>
2019-03-21 22:23:05 -0400

    Add MVC project

---
+++ PropertyManagement.Ui.Mvc/Controllers/BuildingController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PropertyManagement.Domain;
+using PropertyManagement.Repositories.Abstract;
+
+namespace PropertyManagement.Ui.Mvc.Controllers
+{
+    public class BuildingController : Controller
+    {
+        private readonly IBuildingRepository _buildingRepository;
+
+        public BuildingController(IBuildingRepository buildingRepository)
+        {
+            if (buildingRepository == null)
+            {
+                throw new ArgumentNullException("buildingRepository");
+            }
+
+            _buildingRepository = buildingRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var buildings = _buildingRepository.GetBuildings();
+
+            return View(buildings);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var building = new Building();
+
+            if (id > 0)
+            {
+                building = _buildingRepository.GetBuilding(id);
+            }
+
+            return View(building);
+        }
+
+        public IActionResult Save(Building building)
+        {
+            if (building.BuildingId > 0)
+            {
+                _buildingRepository.UpdateBuilding(building);
+            }
+            else
+            {
+                _buildingRepository.AddBuilding(building);
+            }
+
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            _buildingRepository.DeleteBuilding(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
