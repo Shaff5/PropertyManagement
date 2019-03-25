c1af114e6a129f7620bf6a87f2d5d2c6bd27c9da
shaff5 <sean.shaffer@hotmail.com>
2019-03-24 22:12:41 -0400

    Build out data validation

--- PropertyManagement.Ui.Mvc/Models/Building/BuildingBindingModel.cs
+++ PropertyManagement.Ui.Mvc/Models/Building/BuildingBindingModel.cs
@@ -11,28 +11,36 @@ namespace PropertyManagement.Ui.Mvc.Models.Building
         public int BuildingId { get; set; }
 
         [Editable(false)]
+        [Display(Name = "Created On")]
         public DateTime CreatedOn { get; set; }
 
         [Editable(false)]
+        [Display(Name = "Created By")]
         public int CreatedBy { get; set; }
 
         [Editable(false)]
+        [Display(Name = "Last Updated On")]
         public DateTime LastUpdatedOn { get; set; }
 
         [Editable(false)]
+        [Display(Name = "Last Updated By")]
         public int LastUpdatedBy { get; set; }
 
+        [Display(Name = "Building Name")]
         [Required]
         [StringLength(75)]
         public string BuildingName { get; set; }
 
+        [Display(Name = "Address Line 1")]
         [Required]
         [StringLength(50)]
         public string AddressLine1 { get; set; }
 
+        [Display(Name = "Address Line 2")]
         [StringLength(50)]
         public string AddressLine2 { get; set; }
 
+        [Display(Name = "Address Line 3")]
         [StringLength(50)]
         public string AddressLine3 { get; set; }
 
@@ -41,26 +49,34 @@ namespace PropertyManagement.Ui.Mvc.Models.Building
         public string City { get; set; }
 
         [Required]
-        [StringLength(2)]
-        [MinLength(2)]
         public string State { get; set; }
 
+        [Display(Name = "Zip Code")]
         [Required]
         [StringLength(10)]
         public string ZipCode { get; set; }
 
+        [Display(Name = "Purchase Date")]
+        [DataType(DataType.Date)]
         [Required]
         public DateTime PurchaseDate { get; set; }
 
+        [Display(Name = "Purchase Price")]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "C")]
         [Required]
         [Range(1, int.MaxValue)]
         public decimal PurchasePrice { get; set; }
 
+        [Display(Name = "Sell Date")]
+        [DataType(DataType.Date)]
         public DateTime? SellDate { get; set; }
 
+        [Display(Name = "Sell Price")]
         [Range(0, int.MaxValue)]
         public decimal? SellPrice { get; set; }
 
+        [Display(Name = "Number of Units")]
         [Required]
         [Range(1, int.MaxValue)]
         public decimal NumberOfUnits { get; set; }
--- PropertyManagement.Ui.Mvc/Models/Building/BuildingViewModel.cs
+++ PropertyManagement.Ui.Mvc/Models/Building/BuildingViewModel.cs
@@ -23,19 +23,25 @@ namespace PropertyManagement.Ui.Mvc.Models.Building
         [Display(Name = "Last Updated By")]
         public string LastUpdatedBy { get; set; }
 
-        [Display (Name = "Building Name")]
+        [Display(Name = "Building Name")]
         [Required]
+        [StringLength(75)]
         public string BuildingName { get; set; }
 
         [Display(Name = "Address Line 1")]
+        [Required]
+        [StringLength(50)]
         public string AddressLine1 { get; set; }
 
         [Display(Name = "Address Line 2")]
+        [StringLength(50)]
         public string AddressLine2 { get; set; }
 
         [Display(Name = "Address Line 3")]
+        [StringLength(50)]
         public string AddressLine3 { get; set; }
 
+        [StringLength(50)]
         public string City { get; set; }
 
         public IEnumerable<SelectListItem> States { get; set; } =
@@ -54,15 +60,20 @@ namespace PropertyManagement.Ui.Mvc.Models.Building
         public string State { get; set; }
 
         [Display(Name = "Zip Code")]
+        [Required]
+        [StringLength(10)]
         public string ZipCode { get; set; }
 
         [Display(Name = "Purchase Date")]
         [DataType(DataType.Date)]
+        [Required]
         public DateTime? PurchaseDate { get; set; }
 
         [Display(Name = "Purchase Price")]
         [DataType(DataType.Currency)]
         [DisplayFormat(DataFormatString = "C")]
+        [Required]
+        [Range(1, int.MaxValue)]
         public decimal PurchasePrice { get; set; }
 
         [Display(Name = "Sell Date")]
@@ -70,9 +81,12 @@ namespace PropertyManagement.Ui.Mvc.Models.Building
         public DateTime? SellDate { get; set; }
 
         [Display(Name = "Sell Price")]
+        [Range(0, int.MaxValue)]
         public decimal? SellPrice { get; set; }
 
         [Display(Name = "Number of Units")]
+        [Required]
+        [Range(1, int.MaxValue)]
         public decimal NumberOfUnits { get; set; }
     }
 }
