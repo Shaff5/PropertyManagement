82ea67a40e9c278d31383c1d8ab68f437d5dc1c6
Sean Shaffer <sshaffer@satuit.com>
2019-04-01 13:37:59 -0400

    Move chart data creation to controller

--- PropertyManagement.Ui.Mvc/Controllers/DashboardController.cs
+++ PropertyManagement.Ui.Mvc/Controllers/DashboardController.cs
@@ -11,7 +11,32 @@ namespace PropertyManagement.Ui.Mvc.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var chartJson = new
+            {
+                chart = new
+                {
+                    caption = "Countries With Most Oil Reserves [2017-18]",
+                    subCaption = "In MMbbl = One Million barrels",
+                    xAxisName = "Country",
+                    yAxisName = "Reserves (MMbbl)",
+                    numberSuffix = "K",
+                    theme = "fusion"
+                },
+                data = new[]
+                {
+                    new {label = "Venezuela", value = "290"},
+                    new {label = "Saudi", value = "260"},
+                    new {label = "Canada", value = "180"},
+                    new {label = "Iran", value = "140"},
+                    new {label = "Russia", value = "115"},
+                    new {label = "UAE", value = "100"},
+                    new {label = "USA", value = "30"},
+                    new {label = "China", value = "30"}
+                }
+            };
+
+            //return Json(chartJson);
+            return View(chartJson);
         }
     }
 }
---
+++ PropertyManagement.Ui.Mvc/Models/Dashboard/ChartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PropertyManagement.Ui.Mvc.Models.Dashboard
+{
+    public class ChartViewModel
+    {
+        public string ChartType { get; set; }
+        public string DataFormat { get; set; }
+        public string RenderAt { get; set; }
+    }
+}
