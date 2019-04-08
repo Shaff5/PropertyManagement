4e3589d7ea8b79a0e542b6285519feaa176bfdcf
Sean Shaffer <sshaffer@satuit.com>
2019-04-08 14:48:39 -0400

    Build fast find functionality

---
+++ PropertyManagement.Ui.Mvc/Controllers/FastFindController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PropertyManagement.Repositories.Abstract;
+using PropertyManagement.Ui.Mvc.Models.FastFind;
+
+namespace PropertyManagement.Ui.Mvc.Controllers
+{
+    public class FastFindController : Controller
+    {
+        IBuildingRepository _buildingRepository;
+        IUnitRepository _unitRepository;
+
+        public FastFindController(IBuildingRepository buildingRepository,
+            IUnitRepository unitRepository)
+        {
+            if (buildingRepository == null)
+            {
+                throw new ArgumentNullException("buildingRepository");
+            }
+            if (unitRepository == null)
+            {
+                throw new ArgumentNullException("unitRepository");
+            }
+
+            _buildingRepository = buildingRepository;
+            _unitRepository = unitRepository;
+        }
+
+        public IActionResult Index(string searchTerm)
+        {
+            var buildingFilters = new List<Tuple<string, object>>();
+            buildingFilters.Add(new Tuple<string, object>("BuildingName LIKE {0}", $"%{searchTerm}%"));
+            var buildings = _buildingRepository.GetBuildings(buildingFilters);
+
+            var unitFilters = new List<Tuple<string, object>>();
+            unitFilters.Add(new Tuple<string, object>("UnitName LIKE {0}", $"%{searchTerm}%"));
+            var units = _unitRepository.GetUnits(unitFilters);
+
+            var viewModel = new FastFindViewModel();
+            viewModel.Buildings = buildings;
+            viewModel.Units = units;
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ PropertyManagement.Ui.Mvc/Models/FastFind/FastFindViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PropertyManagement.Domain;
+
+namespace PropertyManagement.Ui.Mvc.Models.FastFind
+{
+    public class FastFindViewModel
+    {
+        public IQueryable<Domain.Building> Buildings { get; set; }
+        public IQueryable<Domain.Unit> Units { get; set; }
+
+    }
+}
